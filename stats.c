5e3c1101d6c004459d7c8766b105a4b893b3137d
SudakovAndrei <andrejj_vs@mail.ru>
2018-04-16 09:47:36 -0600

    Use test array from ese-coursera-course1 project

--- stats.c
+++ stats.c
@@ -25,25 +25,28 @@
 #include <stdio.h>
 #include "stats.h"
 
-#define array_size 9
+/* Size of the Data Set */
+#define SIZE (40)
 
 int main(void) {
-unsigned char tst_array[array_size] = {
-  0x25, 0x13, 0x48, 0x33, 0x9A, 0xbd, 0xe3, 0xa2, 0x64
-};
-unsigned char sorted_tst_array[array_size];
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+unsigned char sorted_test[SIZE];
 int i;
 
   printf("Print array values unsorted\n");
-  print_array(tst_array, array_size);
+  print_array(test, SIZE);
   printf("Print array values after sort\n");
-  for (i=0; i<array_size; i++){
-    sorted_tst_array[i] = tst_array[i];
+  for ( i=0; i < SIZE; i++){
+    sorted_test[i] = test[i];
   }
-  sort_array(sorted_tst_array, array_size); 
-  print_array(sorted_tst_array, array_size);
+  sort_array(sorted_test, SIZE); 
+  print_array(sorted_test, SIZE);
   printf("Print statistic values of an array \n");
-  print_statistics(tst_array, array_size);
+  print_statistics(test, SIZE);
   
   return 0;
 }
@@ -51,8 +54,8 @@ unsigned char find_maximum(unsigned char * ptr, unsigned int size) {
 unsigned int i;
 unsigned char found_max=0; /* Set to minimum value*/
 
-  for (i=0;i<size;i++){
-    if (found_max < ptr[i]){ /* if array value larger*/
+  for ( i = 0; i < size; i++ ){
+    if ( found_max < ptr[i] ){ /* if array value larger*/
       found_max = ptr[i]; /* store this value*/
     }
   }
@@ -64,8 +67,8 @@ unsigned char find_minimum(unsigned char * ptr, unsigned int size) {
 unsigned int i;
 unsigned char found_min=255; /* Set to maximum value*/
 
-  for (i=0;i<size;i++){
-    if (found_min > ptr[i]){ /* if array value less*/
+  for ( i = 0; i < size; i++){
+    if ( found_min > ptr[i] ){ /* if array value less*/
       found_min = ptr[i]; /* store this value*/
     }
   }
@@ -77,7 +80,7 @@ unsigned char find_median(unsigned char * ptr, unsigned int size) {
 unsigned char found_median;
 
   sort_array(ptr, size); /* call sort function before */
-  if (size % 2){ /* if array size odd */
+  if ( size % 2 ){ /* if array size odd */
     found_median = ptr[size / 2];
   }
   else{ /* if array size even */
@@ -92,7 +95,7 @@ unsigned int i;
 unsigned long array_sum=0;
 unsigned char found_mean;
 
-  for (i=0;i<size;i++){
+  for ( i = 0; i < size; i++){
     array_sum += ptr[i]; /* summarize all the values */
   }
   found_mean = array_sum / size; /* Divide total value by the count */
@@ -111,17 +114,17 @@ unsigned char mean_value;
   median_value = find_median(ptr, size); /* find median value */
   mean_value = find_mean(ptr, size); /* find mean value */
   
-  printf("Maximum array value = 0x%x\n", maximum_value);
-  printf("Minimum array value = 0x%x\n", minimum_value);
-  printf("Median array value = 0x%x\n", median_value);
-  printf("Mean array value = 0x%x\n", mean_value);
+  printf("Maximum array value = %d\n", maximum_value);
+  printf("Minimum array value = %d\n", minimum_value);
+  printf("Median array value = %d\n", median_value);
+  printf("Mean array value = %d\n", mean_value);
 }
 
 void print_array(unsigned char * ptr, unsigned int size) {
 int i;
 
-  for (i=0;i<size;i++) {
-    printf("Array[%d] value = 0x%x\n", i, ptr[i]);
+  for ( i = 0; i < size; i++ ) {
+    printf("Array[%d] value = %d\n", i, ptr[i]);
   }
 }
 
@@ -131,9 +134,9 @@ int j;
 unsigned char tmp1;
 unsigned char tmp2;
   
-  for(i = 0; i < size; i++){
-    for(j = 0; j < size - i - 1; j++){
-      if(ptr[j] > ptr[j + 1]){ /* if current value is greater than  next */
+  for( i = 0; i < size; i++ ){
+    for( j = 0; j < size - i - 1; j++ ){
+      if( ptr[j] > ptr[j + 1] ){ /* if current value is greater than  next */
         /* swap them */
         tmp1 = ptr[j];
         tmp2 = ptr[j + 1];  
